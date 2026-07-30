9c4d67faf55c253e5ce7a1070655cd052870794f
Abigail vanLuling <abigail.vanluling@gmail.com>
2026-07-30 13:47:48 -0600

    learn about conditional logic

--- control_flow_if.py
+++ control_flow_if.py
@@ -6,7 +6,7 @@ my_name = 'Abigail'
 my_gender = 'Female'
 
 if my_gender == 'Female': 
-    print("Hello Ms." + my_name)
+    print("Hello Ms. " + my_name)
 elif my_gender == 'Male':
     print("Hello Mr." + my_name)
 elif my_gender == 'Non-Binary':
@@ -22,3 +22,15 @@ else:
 # on the next line follows an indented code block: 
 # the indented code block is what the machine will run IF the logical statement
 # evaluates to true
+
+# after this code block, you can have between zero and many ELIF statements.
+# structure: elif LOGICAL STATEMENT: 
+# each followed by their own code block:
+# these code blocks will run when the associated logical statement is true AND
+# none of the previous values were true
+
+# conditional logic blocks run sequentially, one statement at a time
+# and they stop at the first True statement that they encounter.
+# the final statement can be (but does not have to be) an ELSE statement:
+# else, note that there is no condition
+# 
